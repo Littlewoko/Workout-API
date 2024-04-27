1509e6b5789c67e1d22023dfc05f27b99800bab3
Littlewoko <56637748+Littlewoko@users.noreply.github.com>
2024-04-27 16:53:39 +0100

    workout class

---
+++ Models/Workout.cs
@@ -0,0 +1,25 @@
+﻿namespace Workout_API.Models
+{
+    /// <summary>
+    /// 
+    /// A workout is a single session consisting of one or more movements
+    /// 
+    /// </summary>
+    public class Workout
+    {
+        public DateTime Date { get; set; }
+        public List<Movement> Movements { get; set; }
+
+        public Workout(DateTime date, List<Movement>? movements) 
+        { 
+            this.Date = date;
+            if(movements != null)
+            {
+                this.Movements = movements;
+            } else
+            {
+                this.Movements = new List<Movement>();
+            }
+        }
+    }
+}
