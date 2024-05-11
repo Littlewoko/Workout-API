22f418245f7e4009c149a1715760517db6daf186
Littlewoko <56637748+Littlewoko@users.noreply.github.com>
2024-05-11 15:21:30 +0100

    created workout controller

---
+++ Controllers/WorkoutController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+using Workout_API.DBContexts;
+using Workout_API.Models;
+
+namespace Workout_API.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        private DBContext _context;
+        public WorkoutController(DBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetWorkout")]
+        public IActionResult Get(int Id)
+        {
+            Workout? workout = null;
+            using (_context)
+            {
+                workout = _context.Workouts.SingleOrDefault(u => u.Id == Id);
+            }
+
+            if (workout == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(workout);
+        }
+
+        [HttpPost(Name = "CreateWorkout")]
+        public IActionResult CreateWorkout([FromBody] Workout newWorkout)
+        {
+            using(_context)
+            {
+                _context.Workouts.Add(newWorkout);
+                _context.SaveChanges();
+            }
+
+            return CreatedAtRoute("GetWorkout", new { newWorkout.Id }, newWorkout);
+        }
+    }
+}
